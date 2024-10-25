c6d1150ad0b9f9ce3e4b0b0e63db3ba1a559d003
Meeri Hurtta <Meeri.Hurtta@metropolia.fi>
2024-10-25 10:13:53 +0300

    wee

---
+++ Matematiikka 2, tehtäviä 1/Teht-1.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+a = 2.493
+b = 0.911
+
+print(np.degrees(a))
+print(np.degrees(b))
+
+c = 137.7
+d = 62.3
+
+print(np.radians(c))
+print(np.radians(d))
+
+e = 30
+f = 45
+g = 60
+h = 90
+i = 120
+j = 135
+k = 150
+l = 180
+m = 270
+o = 360
+
+print(f"{e} astetta on {np.radians(e)} radiaania")
+print(f"{f} astetta on {np.radians(f)} radiaania")
+print(f"{g} astetta on {np.radians(g)} radiaania")
+print(f"{h} astetta on {np.radians(h)} radiaania")
+print(f"{i} astetta on {np.radians(i)} radiaania")
+print(f"{j} astetta on {np.radians(j)} radiaania")
+print(f"{k} astetta on {np.radians(k)} radiaania")
+print(f"{l} astetta on {np.radians(l)} radiaania")
+print(f"{m} astetta on {np.radians(m)} radiaania")
+print(f"{o} astetta on {np.radians(o)} radiaania")
+
